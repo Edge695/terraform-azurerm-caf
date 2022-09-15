952d9c9b433bda416c5235195bc2a6c083373b8e
Edge695 <55743899+Edge695@users.noreply.github.com>
2022-09-15 11:16:17 +0200

    Update virtual_machines_extensions.tf

--- virtual_machines_extensions.tf
+++ virtual_machines_extensions.tf
@@ -54,17 +54,17 @@ module "vm_extension_microsoft_azure_domainjoin" {
   keyvaults          = local.combined_objects_keyvaults
 }
      
-module "vm_extension_aadjoin" {
+module "vm_extension_AADLoginForWindows" {
   source = "./modules/compute/virtual_machine_extensions"
 
   for_each = {
     for key, value in try(local.compute.virtual_machines, {}) : key => value
-    if try(value.virtual_machine_extensions.aadjoin, null) != null
+    if try(value.virtual_machine_extensions.AADLoginForWindows, null) != null
   }
 
   client_config      = local.client_config                  #refer landingzone.tf for the correct module name.
   virtual_machine_id = module.virtual_machines[each.key].id #refer landingzone.tf for the correct module name.
-  extension          = each.value.virtual_machine_extensions.aadjoin
+  extension          = each.value.virtual_machine_extensions.AADLoginForWindows
   extension_name     = "aadjoin"
 }
 
