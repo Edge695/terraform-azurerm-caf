66352de703c0ab9db44a83ce8e2f77b2d528baf9
Edge695 <55743899+Edge695@users.noreply.github.com>
2022-10-12 16:55:57 +0200

    Update backup.tf

--- modules/recovery_vault/backup.tf
+++ modules/recovery_vault/backup.tf
@@ -1,7 +1,7 @@
 
 resource "azurerm_backup_protected_vm" "backup" {
   for_each = try(var.settings.backup, {})
-    vault_key                   = try(var.recovery_vaults[var.client_config.landingzone_key][var.settings.backup.vault_key], null),
+    vault_key                   = try(var.recovery_vaults[var.client_config.landingzone_key][var.settings.backup.vault_key], null)
     source_vm_id                = try(var.settings.backup.source_vm_id, null)
     policy_key                  = try(var.settings.backup.policy_key, null)
 }
