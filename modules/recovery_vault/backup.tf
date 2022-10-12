e3b542b8a4198ebee41efab44587a4ccef21df1d
Edge695 <55743899+Edge695@users.noreply.github.com>
2022-10-12 17:06:41 +0200

    Update backup.tf

--- modules/recovery_vault/backup.tf
+++ modules/recovery_vault/backup.tf
@@ -1,16 +1,26 @@
 
 resource "azurerm_backup_protected_vm" "backup" {
   for_each = try(var.settings.backup, {})
-    resource_group_name         = try(var.recovery_vaults[var.settings.backup.lz_key][var.settings.backup.vault_key].resource_group_name, null)
+  
+  resource_group_name = coalesce(
+    try(var.settings.backup.backup_vault_rg, null),
+    try(split("/", var.settings.backup.backup_vault_id)[4], null),
+    try(var.recovery_vaults[var.client_config.landingzone_key][var.settings.backup.vault_key].resource_group_name, null),
+    try(var.recovery_vaults[var.settings.backup.lz_key][var.settings.backup.vault_key].resource_group_name, null)
+  )
     
-    recovery_vault_name = coalesce(
-      try(var.settings.backup.backup_vault_name, null),
-      try(split("/", var.settings.backup.backup_vault_id)[8], null),
-      try(var.recovery_vaults[var.client_config.landingzone_key][var.settings.backup.vault_key].name, null),
-      try(var.recovery_vaults[var.settings.backup.lz_key][var.settings.backup.vault_key].name, null)
-    )
-    
-    vault_key                   = try(var.recovery_vaults[var.client_config.landingzone_key][var.settings.backup.vault_key], null)
-    source_vm_id                = try(var.settings.backup.source_vm_id, null)
-    policy_key                  = try(var.settings.backup.policy_key, null)
+  recovery_vault_name = coalesce(
+    try(var.settings.backup.backup_vault_name, null),
+    try(split("/", var.settings.backup.backup_vault_id)[8], null),
+    try(var.recovery_vaults[var.client_config.landingzone_key][var.settings.backup.vault_key].name, null),
+    try(var.recovery_vaults[var.settings.backup.lz_key][var.settings.backup.vault_key].name, null)
+  )
+  
+  backup_policy_id = coalesce(
+    try(var.settings.backup.backup_policy_id, null),
+    try(var.recovery_vaults[var.client_config.landingzone_key][var.settings.backup.vault_key].backup_policies.virtual_machines[var.settings.backup.policy_key].id, null),
+    try(var.recovery_vaults[var.settings.backup.lz_key][var.settings.backup.vault_key].backup_policies.virtual_machines[var.settings.backup.policy_key].id, null)
+  )
+  
+  source_vm_id                = try(var.settings.backup.source_vm_id, null)
 }
