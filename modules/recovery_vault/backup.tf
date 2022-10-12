2895619fde7edd7037ac3affa02a00954c888edf
Edge695 <55743899+Edge695@users.noreply.github.com>
2022-10-12 15:09:28 +0200

    Update backup.tf

--- modules/recovery_vault/backup.tf
+++ modules/recovery_vault/backup.tf
@@ -16,7 +16,9 @@ resource "azurerm_backup_protected_vm" "backup" {
     try(var.recovery_vaults[var.settings.backup.lz_key][var.settings.backup.vault_key].name, null)
   )
   
-  source_vm_id = local.os_type == "linux" ? try(azurerm_linux_virtual_machine.vm["linux"].id, null) : try(azurerm_windows_virtual_machine.vm["windows"].id, null)
+  source_vm_id = coalesce(
+     try(var.settings.backup.source_vm_id, null)
+  )
   
   backup_policy_id = coalesce(
     try(var.settings.backup.backup_policy_id, null),
