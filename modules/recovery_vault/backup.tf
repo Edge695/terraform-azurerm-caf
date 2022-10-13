4620ae1f38d11462e17bcc31aa1d8f500196ed81
Edge695 <55743899+Edge695@users.noreply.github.com>
2022-10-13 14:44:40 +0200

    Update backup.tf

--- modules/recovery_vault/backup.tf
+++ modules/recovery_vault/backup.tf
@@ -4,6 +4,5 @@ resource "azurerm_backup_protected_vm" "backup" {
   resource_group_name   = try(var.settings.backup.rg_key.resource_group_name, null)
   recovery_vault_name   = try(var.settings.backup.vault_key.name, null)
   backup_policy_id      = try(var.settings.backup.policy_key.id, null)
-  # backup_policy_id    = try(var.settings.backup.backup_policy_id, null)
-  # source_vm_id        = try(var.settings.backup.source_vm_id, null)
+  source_vm_id          = try(var.settings.backup.source_vm_id, null)
 }
