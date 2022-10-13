a8bf67c7181c573963ade939c9a5dd14ea1806e6
Edge695 <55743899+Edge695@users.noreply.github.com>
2022-10-13 13:35:47 +0200

    Update backup.tf

--- modules/recovery_vault/backup.tf
+++ modules/recovery_vault/backup.tf
@@ -2,12 +2,7 @@
 resource "azurerm_backup_protected_vm" "backup" {
   for_each = try(var.settings.backup, {})
   
-  resource_group_name = coalesce(
-    try(var.settings.backup.backup_vault_rg, null),
-    try(split("/", var.settings.backup.backup_vault_id)[4], null),
-    try(var.recovery_vaults[var.client_config.landingzone_key][var.settings.backup.vault_key].resource_group_name, null),
-    try(var.recovery_vaults[var.settings.backup.lz_key][var.settings.backup.vault_key].resource_group_name, null)
-  )
+  resource_group_name = try(var.settings.backup.vault_key.resource_group_name, null)
   
   recovery_vault_name = try(var.settings.backup.vault_key.name, null)
   backup_policy_id    = try(var.settings.backup.policy_key.id, null)
