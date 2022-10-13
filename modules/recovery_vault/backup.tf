5b36d09dcf6b2eae9b1319bd51bec80b9e2cb5c9
Edge695 <55743899+Edge695@users.noreply.github.com>
2022-10-13 16:41:21 +0200

    Update backup.tf

--- modules/recovery_vault/backup.tf
+++ modules/recovery_vault/backup.tf
@@ -8,10 +8,17 @@ resource "azurerm_backup_protected_vm" "backup" {
     try(var.recovery_vaults[var.client_config.landingzone_key][var.settings.backup.vault_key].resource_group_name, null),
     try(var.recovery_vaults[var.settings.backup.lz_key][var.settings.backup.vault_key].resource_group_name, null)
   )
+
   recovery_vault_name = coalesce(
     try(var.settings.backup.backup_vault_name, null),
     try(split("/", var.settings.backup.backup_vault_id)[8], null),
     try(var.recovery_vaults[var.client_config.landingzone_key][var.settings.backup.vault_key].name, null),
     try(var.recovery_vaults[var.settings.backup.lz_key][var.settings.backup.vault_key].name, null)
+  )
+  
+  backup_policy_id = coalesce(
+    try(var.settings.backup.backup_policy_id, null),
+    try(var.recovery_vaults[var.client_config.landingzone_key][var.settings.backup.vault_key].backup_policies.virtual_machines[var.settings.backup.policy_key].id, null),
+    try(var.recovery_vaults[var.settings.backup.lz_key][var.settings.backup.vault_key].backup_policies.virtual_machines[var.settings.backup.policy_key].id, null)
   )
 }
