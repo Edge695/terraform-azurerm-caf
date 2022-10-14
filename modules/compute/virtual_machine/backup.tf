dd4c285a6af7b6cb3a6de20738ddde69a4576294
Edge695 <55743899+Edge695@users.noreply.github.com>
2022-10-14 14:53:11 +0200

    Update backup.tf

--- modules/compute/virtual_machine/backup.tf
+++ modules/compute/virtual_machine/backup.tf
@@ -1,17 +1,10 @@
 resource "azurerm_backup_protected_vm" "backup" {
-  # for_each = try(var.settings.backup.vmBackup, {})
-  # resource_group_name = try(each.value.rg_key[var.settings.backup.vault_key].resource_group_name, null)
-  # recovery_vault_name = try(each.value.vault_key[var.settings.backup.vault_key].name, null)
-  # backup_policy_id    = try(each.value.policy_key[var.settings.backup.policy_key].id, null)
-  # source_vm_id        = try(each.value.vm_key[var.settings.virtual_machines.vm_key].id, null)
-  
-  
-    for_each = try(var.settings.backup.vmBackup, {})
+  for_each            = try(var.settings.backup.vmBackup, {})
+  resource_group_name = try(each.value.rg_key[var.settings.backup.vault_key].resource_group_name, null)
+  recovery_vault_name = try(each.value.vault_key[var.settings.backup.vault_key].name, null)
+  backup_policy_id    = try(each.value.policy_key[var.settings.backup.policy_key].id, null)
+  source_vm_id        = try(each.value.vm_key[var.settings.virtual_machines.vm_key].id, null)
 
-  resource_group_name = try(var.settings.backup.vault_key[var.settings.backup.vault_key].resource_group_name, null)
-  recovery_vault_name = try(var.settings.backup.vault_key[var.settings.backup.vault_key].name, null)
-  backup_policy_id    = try(var.settings.backup.policy_key[var.settings.backup.policy_key].id, null)
-  source_vm_id        = try(var.settings.backup.vm_key[var.settings.backup.vm_key].id, null)
   
   
   
