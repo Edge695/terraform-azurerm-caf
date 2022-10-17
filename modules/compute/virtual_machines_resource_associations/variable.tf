4565daf339c9e2060fc8c5ccdccf97b60239f4f3
Edge695 <55743899+Edge695@users.noreply.github.com>
2022-10-17 10:24:20 +0200

    Create variable.tf

---
+++ modules/compute/virtual_machines_resource_associations/variable.tf
@@ -0,0 +1,7 @@
+variable "global_settings" {
+  description = "Global settings object (see module README.md)"
+}
+
+variable "client_config" {
+  description = "Client configuration object (see module README.md)."
+}
