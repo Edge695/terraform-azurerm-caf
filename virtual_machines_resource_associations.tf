596a8f67fb5a269cb1399f4fa75392b96aa5099c
Edge695 <55743899+Edge695@users.noreply.github.com>
2022-10-17 08:25:58 +0200

    Update virtual_machines_resource_associations.tf

--- virtual_machines_resource_associations.tf
+++ virtual_machines_resource_associations.tf
@@ -1,7 +1,3 @@
-resource "azurerm_backup_protected_vm" "backup" {
-  # for_each            = try(var.settings.resource_association.vmBackup, {})
-  resource_group_name = var.resource_group_name
-  recovery_vault_name = var.recovery_vault_name
-  backup_policy_id    = var.backup_policy_id
-  source_vm_id        = var.source_vm_id
+module "resource_association" {
+  source = "./modules/compute/virtual_machine/resource_association"
 }
