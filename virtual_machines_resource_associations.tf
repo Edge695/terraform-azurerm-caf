ae08e820779c873856c454404b6e91d3447e56e4
Edge695 <55743899+Edge695@users.noreply.github.com>
2022-10-17 10:12:30 +0200

    Update virtual_machines_resource_associations.tf

--- virtual_machines_resource_associations.tf
+++ virtual_machines_resource_associations.tf
@@ -1,8 +1,5 @@
-module "resource_association" {
+module "resource_associations" {
   source = "./modules/compute/virtual_machine"
-
-  resource_group_name = try(each.value.resource_group_name)
-  recovery_vault_name = try(each.value.recovery_vault_name)
-  source_vm_id        = try(each.value.source_vm_id)
-  backup_policy_id    = try(each.value.backup_policy_id)
+  global_settings = local.global_settings
+  settings        = each.value
 }
