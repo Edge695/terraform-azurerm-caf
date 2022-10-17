74026aeb0ad197b1febef146e7c460b12e8e7b02
Edge695 <55743899+Edge695@users.noreply.github.com>
2022-10-17 10:57:07 +0200

    Update virtual_machines_resource_associations.tf

--- virtual_machines_resource_associations.tf
+++ virtual_machines_resource_associations.tf
@@ -1,7 +1,7 @@
 module "virtual_machines_resource_associations" {
   source = "./modules/compute/virtual_machines_resource_associations"
   
-  for_each = try(var.virtual_machines_resource_associations, {}) : key => value
+  for_each = try(var.vmbackup, {}) : key => value
   settings            = each.value
   global_settings     = local.global_settings
 }
