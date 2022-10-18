841db11f27650ba00aa4ebf9593826d258fd61c1
Edge695 <55743899+Edge695@users.noreply.github.com>
2022-10-18 15:47:35 +0200

    Update virtual_machines_resource_association.tf

--- virtual_machines_resource_association.tf
+++ virtual_machines_resource_association.tf
@@ -1,6 +1,7 @@
 module "virtual_machines_resource_associations" {
   source   = "./modules/compute/virtual_machines_resource_associations"
-  
-  global_settings    = local.global_settings
-  client_config      = local.client_config
+  for_each = var.virtual_machines_resource_associations
+  settings = each.value
+  global_settings = local.global_settings
+  client_config = local.client_config
 }
