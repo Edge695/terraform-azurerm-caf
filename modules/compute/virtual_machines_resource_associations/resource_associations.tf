24aeda8d2156014b9b4e2840b7a3081167cc0505
Edge695 <55743899+Edge695@users.noreply.github.com>
2022-10-18 16:33:30 +0200

    Update resource_associations.tf

--- modules/compute/virtual_machines_resource_associations/resource_associations.tf
+++ modules/compute/virtual_machines_resource_associations/resource_associations.tf
@@ -1,9 +1,5 @@
 resource "azurerm_backup_protected_vm" "backup" {
-  # resource_group_name = module.rg.resource_group_name
-  # recovery_vault_name = try(var.settings.recovery_vault_name)
-  # backup_policy_id    = try(var.settings.backup_policy_id)
-  # source_vm_id        = try(var.settings.source_vm_id)
-    
+   
   resource_group_name = coalesce(
     try(var.settings.backup.backup_vault_rg, null),
     try(split("/", var.settings.backup.backup_vault_id)[4], null),
@@ -11,7 +7,18 @@ resource "azurerm_backup_protected_vm" "backup" {
     try(var.recovery_vaults[var.settings.backup.lz_key][var.settings.backup.vault_key].resource_group_name, null)
   )
   
-  recovery_vault_name = module.recovery_vaults.name
-  backup_policy_id = module.recovery_vaults.backup_policies.id
-  source_vm_id = module.virtual_machine.id
+  recovery_vault_name = coalesce(
+    try(var.settings.backup.backup_vault_name, null),
+    try(split("/", var.settings.backup.backup_vault_id)[8], null),
+    try(var.recovery_vaults[var.client_config.landingzone_key][var.settings.backup.vault_key].name, null),
+    try(var.recovery_vaults[var.settings.backup.lz_key][var.settings.backup.vault_key].name, null)
+  )
+  
+  # source_vm_id = local.os_type == "linux" ? try(azurerm_linux_virtual_machine.vm["linux"].id, null) : try(azurerm_windows_virtual_machine.vm["windows"].id, null)
+  
+  backup_policy_id = coalesce(
+    try(var.settings.backup.backup_policy_id, null),
+    try(var.recovery_vaults[var.client_config.landingzone_key][var.settings.backup.vault_key].backup_policies.virtual_machines[var.settings.backup.policy_key].id, null),
+    try(var.recovery_vaults[var.settings.backup.lz_key][var.settings.backup.vault_key].backup_policies.virtual_machines[var.settings.backup.policy_key].id, null)
+  )
 }
