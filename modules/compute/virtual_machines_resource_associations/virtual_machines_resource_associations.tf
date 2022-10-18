a82324f4a935b20c44dccddbee9bf5c976325a82
Edge695 <55743899+Edge695@users.noreply.github.com>
2022-10-18 14:38:57 +0200

    Update virtual_machines_resource_associations.tf

--- modules/compute/virtual_machines_resource_associations/virtual_machines_resource_associations.tf
+++ modules/compute/virtual_machines_resource_associations/virtual_machines_resource_associations.tf
@@ -1,11 +1,24 @@
-locals {
-  # Need to update the storage tags if the environment tag is updated with the rover command line
-  tags = try(var.settings.tags, null) == null ? null : try(var.settings.tags.environment, null) == null ? var.settings.tags : merge(lookup(var.settings, "tags", {}), { "environment" : var.global_settings.environment })
-}
-
 resource "azurerm_backup_protected_vm" "backup" {
-  resource_group_name = try(var.settings.resource_group_name, null)
-  recovery_vault_name = try(var.settings.recovery_vault_name, null)
-  backup_policy_id    = try(var.settings.backup_policy_id, null)
-  source_vm_id        = try(var.settings.source_vm_id, null)
+  count = try(var.settings.backup, null) == null ? 0 : 1
+
+  resource_group_name = coalesce(
+    try(var.settings.backup.backup_vault_rg, null),
+    try(split("/", var.settings.backup.backup_vault_id)[4], null),
+    try(var.recovery_vaults[var.client_config.landingzone_key][var.settings.backup.vault_key].resource_group_name, null),
+    try(var.recovery_vaults[var.settings.backup.lz_key][var.settings.backup.vault_key].resource_group_name, null)
+  )
+  recovery_vault_name = coalesce(
+    try(var.settings.backup.backup_vault_name, null),
+    try(split("/", var.settings.backup.backup_vault_id)[8], null),
+    try(var.recovery_vaults[var.client_config.landingzone_key][var.settings.backup.vault_key].name, null),
+    try(var.recovery_vaults[var.settings.backup.lz_key][var.settings.backup.vault_key].name, null)
+  )
+  source_vm_id = local.os_type == "linux" ? try(azurerm_linux_virtual_machine.vm["linux"].id, null) : try(azurerm_windows_virtual_machine.vm["windows"].id, null)
+  backup_policy_id = coalesce(
+    try(var.settings.backup.backup_policy_id, null),
+    try(var.recovery_vaults[var.client_config.landingzone_key][var.settings.backup.vault_key].backup_policies.virtual_machines[var.settings.backup.policy_key].id, null),
+    try(var.recovery_vaults[var.settings.backup.lz_key][var.settings.backup.vault_key].backup_policies.virtual_machines[var.settings.backup.policy_key].id, null)
+  )
+
+  # tags                = local.tags      # Commented - forcing a plan to create some diff as the tag is not handled properly in 2.37.0
 }
