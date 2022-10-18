e32efc037c742bafcbfbff7d00602a4bcfd5a353
Edge695 <55743899+Edge695@users.noreply.github.com>
2022-10-18 14:20:15 +0200

    Update virtual_machines_resource_associations.tf

--- virtual_machines_resource_associations.tf
+++ virtual_machines_resource_associations.tf
@@ -1,7 +1,7 @@
 module "virtual_machines_resource_associations" {
   depends_on = [module.virtual_machines, module.recovery_vaults]
   source   = "./modules/compute/virtual_machines_resource_associations"
-  for_each = var.virtual_machines_resource_associations
+  for_each = var.resource_associations
 
   global_settings    = local.global_settings
   client_config      = local.client_config
