644eef7355fc63a7b55913357145566cae5c86c5
Edge695 <55743899+Edge695@users.noreply.github.com>
2022-10-18 12:17:40 +0200

    Update virtual_machines_resource_associations.tf

--- virtual_machines_resource_associations.tf
+++ virtual_machines_resource_associations.tf
@@ -1,13 +1,12 @@
 module "virtual_machines_resource_associations" {
-  source = "./modules/compute/virtual_machines_resource_associations"
-  
-  for_each = var.resource_associations
+  source   = "./modules/compute/virtual_machines_resource_associations"
+  for_each = var.virtual_machines_resource_associations
 
-  global_settings     = local.global_settings
-  client_config       = local.client_config
-  # settings            = each.value
+  global_settings    = local.global_settings
+  client_config      = local.client_config
+  settings           = each.valuebase_tags          = try(local.global_settings.inherit_tags, false) ? try(local.combined_objects_resource_groups[try(each.value.resource_group.lz_key, local.client_config.landingzone_key)][try(each.value.resource_group.key, each.value.resource_group_key)].tags, {}) : {}
 }
-
+  
 output "virtual_machines_resource_associations" {
-    value = module.virtual_machines_resource_associations
+  value = module.virtual_machines_resource_associations
 }
