506079d6b686f3cb2ecf5268e5f67dd521f036f3
Edge695 <55743899+Edge695@users.noreply.github.com>
2022-10-18 15:49:56 +0200

    Update variables.tf

--- modules/compute/virtual_machines_resource_associations/variables.tf
+++ modules/compute/virtual_machines_resource_associations/variables.tf
@@ -9,8 +9,3 @@ variable "client_config" {
 variable "settings" {
   description = "Settings object."
 }
-
-variable "resource_group_name" {}
-variable "recovery_vault_name" {}
-variable "source_vm_id" {}
-variable "backup_policy_id" {}
