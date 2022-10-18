6884c8d4a4a6ecfde31311f6f8f583ee6b42408e
Edge695 <55743899+Edge695@users.noreply.github.com>
2022-10-18 15:16:40 +0200

    Update and rename virtual_machines_resource_associations.tf to virtual_machines_resource_association.tf

--- virtual_machines_resource_association.tf
+++ virtual_machines_resource_association.tf
@@ -1,13 +1,8 @@
-module "virtual_machines_resource_associations" {
-  depends_on = [module.virtual_machines, module.recovery_vaults]
+module "virtual_machines_resource_association" {
   source   = "./modules/compute/virtual_machines_resource_associations"
   for_each = var.resource_associations
 
   global_settings    = local.global_settings
   client_config      = local.client_config
   settings           = each.value
-}
-  
-output "virtual_machines_resource_associations" {
-  value = module.virtual_machines_resource_associations
 }
