2ae5a416f5afae85d2f3da114f9c99a54c6831c3
Edge695 <55743899+Edge695@users.noreply.github.com>
2022-10-19 09:26:22 +0200

    Update main.tf

--- modules/compute/virtual_machines_resource_associations/main.tf
+++ modules/compute/virtual_machines_resource_associations/main.tf
@@ -1,12 +1,7 @@
 terraform {
   required_providers {
     azurerm = {
-      source = "hashicorp/azurerm"
-      version = "3.27.0"
+      source  = "hashicorp/azurerm"
+      version = "~> 2.99"
     }
   }
-}
-
-provider "azurerm" {
-  # Configuration options
-}
