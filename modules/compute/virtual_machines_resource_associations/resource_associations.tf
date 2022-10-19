9a41b01076d256db23573a3c15b51eeaf72a9bcb
Edge695 <55743899+Edge695@users.noreply.github.com>
2022-10-19 08:46:41 +0200

    Update resource_associations.tf

--- modules/compute/virtual_machines_resource_associations/resource_associations.tf
+++ modules/compute/virtual_machines_resource_associations/resource_associations.tf
@@ -1,18 +1,7 @@
 resource "azurerm_backup_protected_vm" "backup" {
    
-  resource_group_name = coalesce(
-    try(var.settings.backup.resource_group_name, null),
-  )
-  
-  recovery_vault_name = coalesce(
-    try(var.settings.backup.recovery_vault_name, null),
-  )
-  
-  source_vm_id = coalesce(
-    try(var.settings.backup.source_vm_id, null),
-  )
-   
-  backup_policy_id = coalesce(
-    try(var.settings.backup.backup_policy_id, null),
-  )
+  resource_group_name = try(var.settings.backup.resource_group_name)
+  recovery_vault_name = try(var.settings.backup.recovery_vault_name)
+  source_vm_id = try(var.settings.backup.source_vm_id)
+  backup_policy_id = try(var.settings.backup.backup_policy_id)
 }
