911914fd45bcf9f8ca5056016507945e2059c357
Edge695 <55743899+Edge695@users.noreply.github.com>
2022-11-01 10:18:01 +0100

    Delete virtual_machines_resource_association.tf

--- virtual_machines_resource_association.tf
+++
@@ -1,7 +0,0 @@
-module "virtual_machines_resource_associations" {
-  source   = "./modules/compute/virtual_machines_resource_associations"
-  for_each = var.virtual_machines_resource_associations
-  settings = each.value
-  global_settings = local.global_settings
-  client_config = local.client_config
-}
